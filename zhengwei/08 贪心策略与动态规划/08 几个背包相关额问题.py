1c6d3f88e8ad28e517dadbe59369f44cc68fcad7
yingliufengpeng <2906631584@qq.com>
2019-10-07 11:29:13 +0800

    '完善贪心策略'

--- zhengwei/08 贪心策略与动态规划/08 几个背包相关额问题.py
+++ zhengwei/08 贪心策略与动态规划/08 几个背包相关额问题.py
@@ -1,2 +1,102 @@
 # -*- coding: utf-8 -*-
 
+'''
+    给出n个物体,第i个物体重量为wi, 选择尽量多的物体,使得总重量不超过C
+    完全背包问题
+'''
+
+print('-' * 20)
+Ws = [1, 1, 3, 4, 2, 4, 5, 6, 9]
+print('WS is', Ws)
+
+# 先对这些重量进行排序
+
+Ws2 = sorted(Ws)
+print('Ws2 is', Ws2)
+
+N = len(Ws2)
+index = 0
+C = 10
+
+
+def solve(nums, total):
+    if not nums:
+        return 0
+
+    t = total
+    i = 0
+
+    while i < len(nums):
+
+        if t >= nums[i]:
+            t -= nums[i]
+        else:
+            break
+
+        i += 1
+
+    return i
+
+
+r = solve(Ws2, 4)
+print('r is', r)
+
+'''
+    部分背包问题
+    
+    有N个物体,第i个物体的重量为wi,价值为vi.总重量不超过C的情况下让总价值最高.
+    每个物体都可以之取走一部分,价值和重量按比例计算.
+    
+    求最大总价值
+    
+    注意: 每个物体可以只拿一部分,因此一定可以让总重量恰好为C
+'''
+
+print('-' * 20)
+Ps = [2, 3, 3, 4, 5]
+Ws = [1, 2, 3, 4, 6]
+
+# 计算每个物体的单价
+unit_ps = [p / w for p, w in zip(Ps, Ws)]
+print('unit_ps is', unit_ps)
+
+P_pair3 = zip(Ps, Ws, unit_ps)
+
+
+P_pair3_sort = sorted(P_pair3, key=lambda t: t[2], reverse=True)
+
+print('P_pair3_sort is', P_pair3_sort)
+
+
+def solve2(p_nums, val):
+    if not p_nums:
+        return 0
+
+    index = 0
+    price = 0
+
+    while index < len(p_nums):
+
+        v = p_nums[index][1]
+        cur_p = p_nums[index][0]
+
+        if v <= val:
+            val -= v
+
+            price += cur_p
+
+        else:
+            # 获得该重量的单价是多少
+            unit_p = p_nums[index][2]
+            price += val * unit_p
+
+            break
+
+        index += 1
+
+    return price
+
+
+r2 = solve2(P_pair3_sort, 2)
+
+print('r2 is', r2)
--- zhengwei/08 贪心策略与动态规划/09 涨姿势_乘船问题.py
+++ zhengwei/08 贪心策略与动态规划/09 涨姿势_乘船问题.py
@@ -1,2 +1,82 @@
 # -*- coding: utf-8 -*-
 
+'''
+    有n个人,每个人有各自的重量wi,每艘船的最大承载量均为C,且最多只能乘坐两个人.用最少的
+    船装载所有人.
+
+    贪心策略: 考虑最轻的人i,如果每个人都无法和他一起坐船(重量和超过C),则唯一的方案是每个人
+    做一条船,否则,他应该选择和他一起坐船的最重的那个
+'''
+
+n = 10
+nums = list(range(1, n + 1))
+
+# step1 对重量进行排序的操作
+
+nums_sort = sorted(nums)
+
+
+def solve(nums, val):
+    if not nums:
+        return 0
+
+    res = 0
+
+    while nums:
+        v = nums[0]
+        j = len(nums) - 1
+        while j > 0:
+            v2 = nums[j]
+            if v + v2 <= val:
+                res += 1
+                nums.pop(j)
+                nums.pop(0)
+                break
+
+            j -= 1
+
+        # 没有找到
+        if j == 0:
+            res += len(nums)
+            break
+
+    return res
+
+
+print('sort of nums is', nums_sort)
+r = solve(nums_sort, 10)
+
+print('r is', r)
+
+print('--' * 10)
+
+
+def solve2(nums, val):
+    if not nums:
+        return 0
+
+    cntPerson = len(nums)
+    cntboat = 0
+
+    p1 = 0
+    p2 = len(nums) - 1
+
+    while cntPerson > 0:
+        if nums[p1] + nums[p2] > val:
+            p2 -= 1
+            cntPerson -= 1
+            cntboat += 1
+
+        else:
+            p1 += 1
+            p2 -= 1
+            cntPerson -= 2
+            cntboat += 1
+
+    return cntboat
+
+
+nums_sort2 = sorted(nums)
+r2 = solve2(nums_sort2, 10)
+
+print('r2 is', r2)
